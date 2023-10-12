15afa3baf3af5be6b08e17eaa35115a52bbe3a11
Lanre <owolabilanre2006@gmail.com>
2023-10-12 13:48:32 -0700

    first commit

---
+++ FactorialCalculator.java
@@ -0,0 +1,21 @@
+import java.util.Scanner;
+
+public class FactorialCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a number: ");
+        int number = scanner.nextInt();
+        scanner.close();
+
+        long factorial = calculateFactorial(number);
+        System.out.println("Factorial of " + number + " is: " + factorial);
+    }
+
+    public static long calculateFactorial(int number) {
+        if (number == 0 || number == 1) {
+            return 1;
+        } else {
+            return number * calculateFactorial(number - 1);
+        }
+    }
+}
---
+++ teacher1.java
@@ -0,0 +1,10 @@
+public class teacher1 {
+	public static void main(String[] args) {
+        int sum = 0;
+        
+        for(int i = 1; i <= 10000; i++) {
+        	sum += i;
+        }
+        System.out.println(sum);    
+    }
+}
